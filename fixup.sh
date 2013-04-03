11dcc4de410997bc9ee8799779834b476d54234a
Amir Malik <a@unoc.net>
2013-04-03 11:07:52 -0700

    Add ~/.ssh/config with shared connections.

--- fixup.sh
+++ fixup.sh
@@ -5,3 +5,8 @@ ln -sv dot-files/.[a-f,h-z]* dot-files/.gitconfig dot-files/.gitignore .
 
 cd ~/Library/Developer/Xcode/UserData
 ln -sv ../../../../dot-files/Library/Developer/Xcode/UserData/FontAndColorThemes .
+
+mkdir -p ~/.ssh/sockets
+chmod 700 ~/.ssh
+cd ~/.ssh
+ln -sv ../dot-files/ssh_config config
