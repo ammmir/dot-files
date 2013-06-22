cef63c72bd9288f2d5023d3a492d39f60b760c65
Amir Malik <a@unoc.net>
2013-06-22 16:09:24 -0700

    Minor fix.

--- fixup.sh
+++ fixup.sh
@@ -3,8 +3,7 @@
 cd ..
 ln -sv dot-files/.[a-f,h-z]* dot-files/.gitconfig dot-files/.gitignore .
 
-cd ~/Library/Developer/Xcode/UserData
-ln -sv ../../../../dot-files/Library/Developer/Xcode/UserData/FontAndColorThemes .
+cd ~/Library/Developer/Xcode/UserData && ln -sv ../../../../dot-files/Library/Developer/Xcode/UserData/FontAndColorThemes .
 
 mkdir -p ~/.ssh/sockets
 chmod 700 ~/.ssh
